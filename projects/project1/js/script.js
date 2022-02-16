2bb9f6f49915f40ff8daaf20951b8c04a119f92f
Radz2122 <70171361+Radz2122@users.noreply.github.com>
2022-02-16 14:25:08 -0500

    p1-background and foreground img setup

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,19 +1,37 @@
 /**
-Title of Project
-Author Name
+Project 1: A Night at the Movies
+Radhika Patel
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
 "use strict";
+// Constants for image loading
+const BCKGRD_IMG = `assets/images/backgroundImg.png`;
+let bckgImg;
+const MYLES_IMG = `assets/images/myles.png`;
+//object that represents Myles
+let mylesImg = {
+  image: undefined,
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  sizeX: 77,
+  sizeY: 167,
+};
 
 
 /**
 Description of preload
 */
 function preload() {
-
+   bckgImg = loadImage(BCKGRD_IMG);
+   mylesImg.image= loadImage(MYLES_IMG);
+   mylesImg.x=windowWidth/2.1;
+   mylesImg.y=windowHeight/1.8;
 }
 
 
@@ -21,6 +39,7 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
 }
 
@@ -29,5 +48,7 @@ function setup() {
 Description of draw()
 */
 function draw() {
-
+image(bckgImg, 0, 0, windowWidth, windowHeight);
+// image(mylesImg, windowWidth/2, windowHeight/2, 100, 50);
+image(mylesImg.image, mylesImg.x, mylesImg.y, mylesImg.sizeX, mylesImg.sizeY);
 }
