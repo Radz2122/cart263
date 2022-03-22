2adc750e0dbd97135c34c448831ac32ab84bab96
Radz2122 <70171361+Radz2122@users.noreply.github.com>
2022-03-22 16:24:58 -0400

    P2- game dimensions setup

--- projects/project2/prototype/js/main.js
+++ projects/project2/prototype/js/main.js
@@ -8,19 +8,32 @@ author, and this description to match your project!
 
 "use strict";
 
+//dimensions of the game
+let width=640;
+let height=960;
+
 //create a JavaScript object to configure Phaser 3 game
 let config = {
   //The type of display
   type: Phaser.AUTO,
-  // dimensions of game's display area
-  width: 800,
-  height: 600,
+  // dimensions of game's display area (mobile) that will scale and be centered
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: width,
+    height: height,
+  },
   physics: {
     default: 'arcade',
   },
   //  array of the different scenes
-  scene: [Boot,Play]
+  scene: [Boot,Play],
+    //since it will be a mobile game, limit the amount of pointers
+  input:{
+    activePointers:1
+  }
 };
+
 
 //create the game using this configuration
 let game = new Phaser.Game(config);
